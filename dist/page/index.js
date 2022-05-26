d674a39d348d20d7b189168a8986d5072e6a1021
gomez-fernando <ferdgomez@hotmail.es>
2022-05-26 07:16:44 +0200

    Add seriesList component, and render it 2 times witn conditional

--- dist/page/index.js
+++ dist/page/index.js
@@ -2,6 +2,7 @@
 // import { HeaderMainSection } from '../components/headerMainSection.js';
 import { SeriesList } from "../components/seriesList.js";
 export function index() {
-    new SeriesList('.series-list');
+    new SeriesList('section.series-pending', false);
+    new SeriesList('section.series-watched', true);
     // new HeaderMainSection('div.container > section.series');
 }
--- src/page/index.ts
+++ src/page/index.ts
@@ -5,6 +5,7 @@ import { SeriesList } from "../components/seriesList.js";
 
 
 export function index() {
-    new SeriesList('.series-list');
+    new SeriesList('section.series-pending', false);
+    new SeriesList('section.series-watched', true);
     // new HeaderMainSection('div.container > section.series');
 }
