e5fc4532a0e7255cc9128c314637b241f665e55c
gomez-fernando <ferdgomez@hotmail.es>
2022-05-26 17:37:09 +0200

    Add interface file

---
+++ src/interfaces/interface.ts
@@ -0,0 +1,10 @@
+export interface serieList {
+    id: number;
+    name: string;
+    creator: string;
+    year: number;
+    poster: string;
+    watched: boolean;
+    score: number;
+    emmies: number;
+}
